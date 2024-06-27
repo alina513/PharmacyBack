6decbe9f284f0dda8d8b25592977219ebc714d46
alina513 <130853290+alina513@users.noreply.github.com>
2024-06-27 16:08:25 +0300

    Update authenticate.js

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -6,6 +6,7 @@ dotenv.config();
 
 const {SECRET_KEY} = process.env;
 
+
 const authenticate = async (req, res, next) => {
     const {authorization = ""} = req.headers;
     const [bearer, token] = authorization.split(" ");
