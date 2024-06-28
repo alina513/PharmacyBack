3c93fdb39c6d4235ef1a5fadb0a2b8631dd43345
alina513 <130853290+alina513@users.noreply.github.com>
2024-06-28 17:47:13 +0300

    Update productsRouter.js

--- routes/productsRouter.js
+++ routes/productsRouter.js
@@ -2,14 +2,13 @@ const express = require("express");
 
 const ctrl = require("../controllers/productsControllers");
 const isValidId = require("../helpers/isValidId.js");
-const authenticate = require("../middlewares/authenticate.js")
 
 
 const productsRouter = express.Router();
 
-productsRouter.get("/", authenticate, ctrl.getAllProducts);
+productsRouter.get("/", ctrl.getAllProducts);
 
-productsRouter.get("/:id", authenticate, isValidId, ctrl.getOneProduct);
+productsRouter.get("/:id", isValidId, ctrl.getOneProduct);
 
 
 
